9e329d1bd3882df3f9124944a335dd45a1e436e4
sharknia <furychick0@gmail.com>
2023-11-26 22:57:04 +0900

    init

--- pomodoro timer/ContentView.swift
+++ pomodoro timer/ContentView.swift
@@ -1,23 +1,70 @@
-//
-//  ContentView.swift
-//  pomodoro timer
-//
-//  Created by 이한결 on 11/26/23.
-//
 
 import SwiftUI
 
+enum TimerState {
+    case running
+    case paused
+    case stopped
+}
+
+
 struct ContentView: View {
+    @StateObject private var timerManager = TimerManager()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("남은 시간: \(timerManager.secondsLeft)초")
+                .font(.largeTitle)
+                .padding()
+
+            if timerManager.timerState == .stopped {
+                Button("25분 집중 시작") {
+                    timerManager.setTimer(minutes: 25)
+                }
+                .customButtonStyle(backgroundColor: .green)
+
+                Button("5분 휴식 시작") {
+                    timerManager.setTimer(minutes: 5)
+                }
+                .customButtonStyle(backgroundColor: .blue)
+
+                Button("20분 긴휴식 시작") {
+                    timerManager.setTimer(minutes: 20)
+                }
+                .customButtonStyle(backgroundColor: .orange)
+            } else {
+                Button(action: {
+                    if timerManager.timerState == .running {
+                        timerManager.pauseTimer()
+                    } else {
+                        timerManager.startTimer()
+                    }
+                }) {
+                    Label(timerManager.timerState == .running ? "일시 정지" : "다시 시작", systemImage: timerManager.timerState == .running ? "pause.circle" : "play.circle")
+                }
+                .customButtonStyle(backgroundColor: .yellow)
+
+                Button("리셋") {
+                    timerManager.resetTimer()
+                }
+                .customButtonStyle(backgroundColor: .red)
+            }
         }
-        .padding()
+        .frame(width: 300, height: 200)
     }
 }
+
+extension View {
+    func customButtonStyle(backgroundColor: Color) -> some View {
+        self.padding(10)
+            .frame(minWidth: 100, maxWidth: .infinity)
+            .background(backgroundColor)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .font(.headline)
+    }
+}
+
 
 #Preview {
     ContentView()
---
+++ pomodoro timer/TimerManager.swift
@@ -0,0 +1,43 @@
+//
+//  TimerManager.swift
+//  pomodoro timer
+//
+//  Created by 이한결 on 11/26/23.
+//
+
+import Foundation
+
+class TimerManager: ObservableObject {
+    @Published var secondsLeft = 0
+    @Published var timerState = TimerState.stopped
+    var timer: Timer?
+
+    func setTimer(minutes: Int) {
+        secondsLeft = minutes * 60
+        startTimer()
+    }
+
+    func startTimer() {
+        timerState = .running
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+
+            if self.secondsLeft > 0 {
+                self.secondsLeft -= 1
+            } else {
+                self.resetTimer()
+            }
+        }
+    }
+
+    func pauseTimer() {
+        timerState = .paused
+        timer?.invalidate()
+    }
+
+    func resetTimer() {
+        timerState = .stopped
+        timer?.invalidate()
+        secondsLeft = 0 // 타이머 초기 시간으로 재설정
+    }
+}
